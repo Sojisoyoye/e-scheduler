eba56bea06ba6bff294a07d91a7e44e31baf377c
Sojisoyoye <soji.soyoye@gmail.com>
2019-12-16 10:44:40 +0100

    reusing components with content projection - content projection - mutiple slopt content protection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AuthService } from './user/auth.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CreateSessionComponent } from './events/event-details/create-session/create-session.component';
 import { SessionListComponent } from './events/event-details/session-list/session-list.component';
+import { CollapsibleWellComponent } from './events/common/collapsible-well/collapsible-well.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { SessionListComponent } from './events/event-details/session-list/sessio
     CreateEventComponent,
     ErrorComponent,
     CreateSessionComponent,
-    SessionListComponent
+    SessionListComponent,
+    CollapsibleWellComponent
   ],
   imports: [
     BrowserModule,
