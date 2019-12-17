2ab40054131b87076a434e0ea63dfb110db565bd
Sojisoyoye <soji.soyoye@gmail.com>
2019-12-17 15:42:25 +0100

    understanding angular's dependency injection - injectionToken and inject decorator - useClass, useValue, useExisting, useFactory

--- src/app/events/common/toastr.service.ts
+++ src/app/events/common/toastr.service.ts
@@ -1,27 +1,14 @@
-import { Injectable } from '@angular/core';
+import { InjectionToken } from '@angular/core';
 
-declare let toastr: any;
-
-@Injectable({
-  providedIn: 'root'
-})
-export class ToastrService {
-
-  constructor() { }
-
-  success(message: string, title?: string) {
-    toastr.success(message, title);
-  }
-
-  info(message: string, title?: string) {
-    toastr.info(message, title);
-  }
-
-  warning(message: string, title?: string) {
-    toastr.warning(message, title);
-  }
-
-  error(message: string, title?: string) {
-    toastr.error(message, title);
-  }
+export let TOASTR_TOKEN = new InjectionToken<Toastr>('toastr');
+export interface Toastr {
+  success(msg: string, title?: string): void;
+  info(msg: string, title?: string): void;
+  warning(msg: string, title?: string): void;
+  error(msg: string, title?: string): void;
 }
+
+
+// Injection token allows us to create a key we can pass into our component like a service
+// to use as a dependency injector withput creating a class, the injection token's job is to create a token
+// used for the dependency injection registry in order to find the instance of the service we want
--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormControl, FormGroup, Form, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { TOASTR_TOKEN, Toastr } from 'src/app/events/common/toastr.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -12,7 +13,11 @@ export class ProfileComponent implements OnInit {
   private firstName: FormControl;
   private lastName: FormControl;
 
-  constructor(private auth: AuthService, private router: Router) { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    @Inject(TOASTR_TOKEN) private toastr: Toastr
+    ) { }
 
   ngOnInit() {
     this.firstName = new FormControl(this.auth.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')]);
@@ -34,7 +39,7 @@ export class ProfileComponent implements OnInit {
 
   saveProfile(formValues) {
     this.auth.updateCurrentUser(formValues.firstName, formValues.lastName);
-    this.router.navigate(['events']);
+    this.toastr.success('Profile Saved');
   }
 
   cancel() {
